bafb68864a537a5ae917c4c9afc8bcdaf41e1acf
nataliagarcia <nataliagarkcia@hotmail.com>
2016-11-08 19:14:46 -0500

    add css, fix javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,42 +3,46 @@
 
 
 //sets letters to choose//
-var computerChoices = ['a' , 'b' , 'c' , 'd ', 'e' ,'f' , 'g', 'h' , 'i' , 'j', 'k' , 'l', 'm', 'n' ,'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' ,'w' , 'x' , 'y' , 'z'];
+var computerChoices = ['a' , 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-
-var guessesLeft = 9;
+//global variables
+var guessesLeft = 20;
 var wins = 0;
 var losses = 0;
+//empty array that will contain the guessed letters
+var lettersGuessed = [];
 
 
 
 //starts the game when user presses a letter//
 document.onkeyup = function(event){
 
+	//this will create a random choise of the letter to be guess
+	var letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	 console.log(letterToGuess);
+
 	 var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	 console.log(userGuess);
 
-	 var letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 	 
-	 var lettersGuessed = userGuess ;
+	 
+	// var lettersGuessed = userGuess ;
 
+	//this foor lop will create score and wil endthe game if user runs off guesses
 	 if(userGuess == letterToGuess) {
 	 	wins++;
-	 }
+	 	lettersGuessed.push(letterToGuess);
 
-     else if (losses < 9){
-	 	losses ++; 
-
-	 }
-	 else {
-	 	lettersGuessed++;
+	 } else if (losses == 20){
+	 	alert("you lost the game");
+	 } else {
 	 	guessesLeft--;
-	 }
-
-	 
+	 	losses ++;
+	  }
+	
 
 //taking tallies and display in hmtl//
- 	var html = "<p>Guess what letter i'm thinking of</p>" +
-		"<p>wins: " + 
+ 	var html = "<p>wins: " + 
 		wins + 
 		"</p>" +
 		"<p>losses: " + 
